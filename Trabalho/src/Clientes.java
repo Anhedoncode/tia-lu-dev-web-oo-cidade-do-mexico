d62bf20045d6a7f6320795999695441d1f9eb24d
brandaokratos-arch <brandaokratos@gmail.com>
2025-08-29 16:29:38 -0300

    Update Clientes.java

--- Trabalho/src/Clientes.java
+++ Trabalho/src/Clientes.java
@@ -1,2 +1,53 @@
-public class Clientes {
+public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(int id, String nome, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
 }
